9911a0ad05890a5f4e961a2a403c73d806a37823
Mani Tripathi <manitripathi.023@gmail.com>
2025-08-10 03:27:19 +0530

    green test when input string is 1 and output is 1

--- src/Components/StringCalculator.jsx
+++ src/Components/StringCalculator.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 
 const StringCalculator = () => {
   const [result, setResult] = useState(null);
+  const [input, setInput] = useState("");
+
+  const handleInputField = (e) => {
+    setInput(e.target.value);
+  };
 
   const handleCalculate = () => {
-    setResult(0);
+    setResult(Number(input));
   };
 
   return (
@@ -14,6 +19,8 @@ const StringCalculator = () => {
         placeholder="enter numbers"
         data-testid="input-field"
         name="input"
+        value={input}
+        onChange={handleInputField}
       />
       <br /> <br />
       <button type="button" onClick={handleCalculate}>
