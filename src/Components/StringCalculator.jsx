fb62d7f34c6c387426b7d3e477ebba4c8b0ab968
Mani Tripathi <manitripathi.023@gmail.com>
2025-08-10 02:50:43 +0530

    built basic UI structure

--- src/Components/StringCalculator.jsx
+++ src/Components/StringCalculator.jsx
@@ -1,5 +1,16 @@
 const StringCalculator = () => {
-  return <div></div>;
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="enter numbers"
+        data-testid="input-field"
+        name="input"
+      />
+      <br /> <br />
+      <button type="button">Calculate</button>
+    </div>
+  );
 };
 
 export default StringCalculator;
